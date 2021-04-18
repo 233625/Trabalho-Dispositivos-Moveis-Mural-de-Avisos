48e0ec8620e97a48e3919a146ea32e28caedb958
Guilherme Almeida <guisilvaalmeida40@hotmail.com>
2021-04-18 20:21:25 -0300

    Criado o botão de exclusão

--- App.js
+++ App.js
@@ -1,36 +1,83 @@
-import React, { useState } from 'react'; 
+import React, { useCallback, useState } from 'react'; 
 import {View, Text, StyleSheet, TouchableOpacity, Button,
-   SafeAreaView, FlatList, Modal} from 'react-native';
+   SafeAreaView, FlatList, Modal, TextInput} from 'react-native';
 import  TarefaLista from './src/components/TarefaLista';
 
    
 export default function App(){
-  const [tarefa,setTarefa] = useState([
-    {key:1, tarefa: 'Comprar mingau'},
-    {key:2, tarefa: 'Comprar Papel'},
-    {key:3, tarefa: 'Comprar Bial'}
-   ]);
+  const [tarefa,setTarefa] = useState([]);
+  const [open,setOpen] = useState(false);
+  const [input, setInput] = useState('');
 
-   const [open,setOpen] = useState(false);
+
+  function Add(){
+    if(input === '')return;
+
+    const data = {
+      key: input,
+      tarefa: input
+    };
+
+    setTarefa([...tarefa,data]);
+    setOpen(false);
+    setInput('');
+  }
+
+  const handleDelete = useCallback((data)=>{
+    const find = tarefa.filter(r => r.key !== data.key);
+    setTarefa(find);
+  })
+
   return(
     //Inicio -> onde fica o titulo
     <SafeAreaView style={styles.conteiner}>
         <View>
           <Text style={styles.title}>Mural da Avisos</Text>
         </View>
-
+       
+       
+        {/*Conecta com o .scr/index.js para aparece os avisos na tela  */}
         <FlatList
         showsHorizontalScrollIndicator={false}
         data={tarefa}
         keyExtractor={(item)=> String(item.key)}
-        renderItem={({item})=> <TarefaLista data={item}/>}
+        renderItem={({item})=> <TarefaLista data={item} handleDelete={handleDelete}/>}
         />
-
+        
+        
+        {/*Espaço feito para a area emm que vai criar as novas tarefas */}
         <Modal transparent={false} visible={open}>
-          <SafeAreaView>
-            <Text>Modal 1221212</Text>
+          <SafeAreaView style={styles.conteiner}>
+          
+            <TouchableOpacity onPress={() => setOpen(false)} >
+              <Text> Voltar </Text>
+            </TouchableOpacity>
+
+            
+            <Text style={styles.title}>Novo Aviso</Text>
+          
+            
+            
+            <View style={styles.corpo}>
+              <TextInput
+              multiline={true}
+              placeholderTextColor="#747474"
+              autoCorrect={false}
+              placeholder="Digite aqui ?"
+              value={input}
+              onChangeText={(texto)=> setInput(texto)}
+              style={styles.input}
+              />
+            </View>
+
+            
+
+            <TouchableOpacity style={styles.corpoBtn} onPress={Add}>
+              <Text style={styles.btn}>Cadastrar</Text>
+            </TouchableOpacity>
           </SafeAreaView>
         </Modal>
+        
         
         {/*Inicio dos comandos para o botão lateral */}
         <TouchableOpacity style={styles.botao}>
@@ -67,5 +114,31 @@ botao:{
   bottom: 25,
   top:545,
   zIndex:9,
+},
+corpo:{
+  marginTop: 15,
+},
+input:{
+  fontSize:15,
+  marginLeft:10,
+  marginRight: 10,
+  marginTop: 30,
+  backgroundColor:'#FFF',
+  padding: 9,
+  height: 85,
+  textAlignVertical: 'top',
+  color:'#000',
+  borderRadius: 5
+},
+btn:{
+  backgroundColor:'#00FF00',
+  marginTop: 10,
+  justifyContent:'center',
+  textAlign: 'center',
+  marginLeft: 10,
+  marginRight: 10,
+  height: 40,
+  borderRadius: 5,
+  fontSize: 20
 }
 });
--- src/components/TarefaLista/index.js
+++ src/components/TarefaLista/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { View, Text,StyleSheet, TouchableOpacity} from 'react-native';
 
-export default function TarefaLista({data}){
+export default function TarefaLista({data, handleDelete}){
     return(
         <View style={styles.conteiner}>
             <TouchableOpacity>
                 <Text style={styles.tarefa}>{data.tarefa}</Text>
+                
+                <TouchableOpacity >
+                    <Text style={styles.excluir} onPress={() =>handleDelete(data)}>excluir</Text>
+                </TouchableOpacity>
             </TouchableOpacity>
+           
+            
         </View>
     )
 }
@@ -32,7 +38,8 @@ tarefa:{
     color:'#121212',
     fontSize: 16,
     paddingLeft:10,
-
-
-}
+},
+//excluir:{
+  //  color:'#ff9999',
+//}
 });
