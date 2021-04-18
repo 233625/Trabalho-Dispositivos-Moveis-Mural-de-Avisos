479d59353972e68a15b4865210cd34600a3af7f6
Guilherme Almeida <guisilvaalmeida40@hotmail.com>
2021-04-17 21:44:12 -0300

    Adicionado botão lateral para a criação de avisos

--- App.js
+++ App.js
@@ -1,22 +1,53 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { Component } from 'react'; 
+import {View, Text, StyleSheet, TouchableOpacity, Button, SafeAreaView} from 'react-native';
 
-const App = () => {
-  return(
-    <View style={styles.conteiner}>
-      <Text style={styles.title}>Mural de Avisos</Text>
-    </View>
-  );
+class App extends Component{
+  aperte(){
+    alert("Ta funcionando!!!!!");
+  }
+  
+  render(){
+    return(
+      <SafeAreaView style={styles.conteiner}>
+          <View>
+            <Text style={styles.title}>Mural da Avisos</Text>
+          </View>
+          
+          <TouchableOpacity style={styles.botao}>
+            <Button
+            style={styles.btnText}
+            color='#00FF00'
+            title="+"
+            onPress={this.aperte}
+            />
+          </TouchableOpacity>
+
+        
+      </SafeAreaView>
+      
+    );
+  }
 }
 
 const styles = StyleSheet.create({
 conteiner:{
-  flex: 1,
-  backgroundColor:'#35AAFF',
+flex: 1,
+backgroundColor:'#35AAFF',
 },
 title:{
-  fontSize: 25,
-  fontStyle: 'italic'
+fontSize: 25,
+fontStyle: 'italic',
+textAlign:'center',
+marginTop: 10,
+},
+botao:{
+  position:'absolute',
+  width:60,
+  height:60,
+  right: 10,
+  bottom: 25,
+  top:545,
+  zIndex:9,
 }
 });
 
